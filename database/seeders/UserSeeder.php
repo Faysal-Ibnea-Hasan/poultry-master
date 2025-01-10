47ffe902f133548001bbf8c4155e5a691fa8db04
Jesan <foysal.jesan@gmail.com>
2025-01-10 16:49:20 +0600

    Intial git

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Carbon\Carbon;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::factory()->create([
+            'name' => 'admin@poultry_master',
+            'email' => 'admin@poultry.com',
+            'password' => Hash::make('admin123'),
+            'email_verified_at' => Carbon::now(),
+        ]);
+    }
+}
