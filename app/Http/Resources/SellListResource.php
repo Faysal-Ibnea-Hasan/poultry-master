b5eb35105ba872368cd402f49b124afbd2d4ac0e
Jesan <foysal.jesan@gmail.com>
2025-04-11 21:39:16 +0600

    MODIFIED: SellListResource

--- app/Http/Resources/SellListResource.php
+++ app/Http/Resources/SellListResource.php
@@ -15,18 +15,18 @@ class SellListResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            "id" => (string)$this->id,
-            "batch_id" => (string)$this->batch_id,
-            "batch_number" => (string)$this->batch?->batch_number,
-            "customer_name" => (string)$this->customer_name,
-            "sale_date" => (string)$this->sale_date,
-            "sell_description" => (string)$this->sellLine?->sell_description,
-            "sell_type" => (string)$this->sellLine?->product_type,
-            "quantity" => (string)$this->sellLine?->quantity,
-            "unit_price" => (string)$this->sellLine?->unit_price,
-            "total_weight" => (string)$this->sellLine?->total_weight,
-            "avg_weight" => (string)($this->sellLine?->total_weight / $this->sellLine?->quantity),
-            "amount" => (string)$this->sellLine?->amount,
+            "id" => (string)$this->sell?->id,
+            "batch_id" => (string)$this->sell?->batch_id,
+            "batch_number" => (string)$this->sell?->batch?->batch_number,
+            "customer_name" => (string)$this->sell?->customer_name,
+            "sale_date" => (string)$this->sell?->sale_date,
+            "sell_description" => (string)$this->sell_description,
+            "sell_type" => (string)$this->product_type,
+            "quantity" => (string)$this->quantity,
+            "unit_price" => (string)$this->unit_price,
+            "total_weight" => (string)$this->total_weight,
+            "avg_weight" => (string)($this->total_weight / $this->quantity),
+            "amount" => (string)$this->amount,
         ];
     }
 }
